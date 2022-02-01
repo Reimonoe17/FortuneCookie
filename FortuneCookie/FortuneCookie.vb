b393c4a8721893f266c43320734d2e7af5b30434
Jamison Burton <jamison@burtontalk.com>
2022-02-01 08:34:43 -0700

    Add project files.

---
+++ FortuneCookie/FortuneCookie.vb
@@ -0,0 +1,17 @@
+﻿'Jamison Burton
+'RCET0265
+'Spring 2022
+'Fortune Cookie
+'https://github.com/Reimonoe17/MultiplicationTable
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module FortuneCookie
+
+    Sub Main()
+
+    End Sub
+
+End Module
